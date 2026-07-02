e6c58f0b8ff3ea62e2dae3580bd2c921f5c73fc8
clearwaytoolsonline-svg <clearwaytoolsonline@gmail.com>
2026-07-02 16:09:28 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,15 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Clearway Safety | Leading Safety Equipment Supplier in Abu Dhabi UAE',
+  description: 'Wholesale & Retail Supplier of Premium Safety Tools, PPE, Traffic Safety Cones, and Industrial Safety Equipments across Abu Dhabi and UAE.',
+  keywords: 'Safety Equipment Abu Dhabi, PPE Supplier UAE, Traffic Safety Products Abu Dhabi, Wholesale Safety Tools',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
